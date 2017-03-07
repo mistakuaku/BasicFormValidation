78a72f22bb5a28d9b87e35130158c507f1b561fa
mistakuaku <dragonz139@hotmail.com>
2017-03-07 12:54:14 -0800

    added the success conditions

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,37 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
+  if (userEntered.length < 6) {
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  else {
+    //document.getElementById("usernameError").innerHTML="Success.";
+    document.getElementById("usernameError").classList.add("hidden-message");
+    document.getElementById("usernameError").classList.remove("shown-message");
+    document.getElementById("usernameGroup").classList.remove("has-error");
+    document.getElementById("usernameGroup").classList.add("has-success");
 
+  }
+
+  if (passEntered == "password"){
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
+else {
+  //document.getElementById("passwordError").innerHTML="Success.";
+  document.getElementById("passwordError").classList.add("hidden-message");
+  document.getElementById("passwordError").classList.remove("shown-message");
+  document.getElementById("passwordGroup").classList.remove("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
+}
+
 }
